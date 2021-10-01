3e03787a10025093871a0239a01fe9c927ceee34
joao99sb <joaovictor665@gmail.com>
2021-09-30 21:24:26 -0300

    wifi connection implementation for real machines

--- OwnLinuxDistro/script_0.sh
+++ OwnLinuxDistro/script_0.sh
@@ -1,5 +1,38 @@
 #!/usr/bin/env bash
 
+
+############################
+#       wifi search        #
+############################
+
+
+# show interfaces devices
+iw dev 
+echo -e "\n"
+echo -e "chose your network interface: "
+
+read NETWORK_INTERFACE
+
+ip link set $NETWORK_INTERFACE up || rfkill unblock wifi && ip link set $NETWORK_INTERFACE up
+
+
+iw dev $NETWORK_INTERFACE scan | grep SSID
+
+echo -e "chose your network: "
+
+read WIFI
+
+echo "password: "
+
+read -s PASSWORD
+
+iwctl --passphrase "${PASSWORD}" station "${NETWORK_INTERFACE}" connect "${WIFI}"
+
+# I need to implement retry password as new feature
+
+
+
+
 ############################
 #     Update pacman        #
 ############################
@@ -17,9 +50,31 @@ loadkeys br-abnt2
 ################################
 
 DISK=/dev/sda
-
 # make function to do the partition
 # 1) show free space    fdisk -l <disk>
 # 2) show partintions   lsblk <disk>
-# 3) create partiiton   
+# 3) create partiiton
 
+partition_function() {
+    local isRunning=1
+
+    if [ $1 -eq 5]
+    then
+        $isRunning=0
+    fi
+
+    while  [ ! $isRunning -eq 0]
+    do
+        echo "chose command "
+        echo "0) to see free space"
+        echo "1) to show partiotions"
+        echo "5) to exit"
+        
+        read param
+        partition_function $param
+
+    done
+
+}
+
+echo "chose: ":
