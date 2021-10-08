20f9542bef2f96baeb57691b5b8b28ac50f7fc2c
joao99sb <joaovictor665@gmail.com>
2021-10-07 22:28:00 -0300

    ntools fix

--- OwnLinuxDistro/script_1.sh
+++ OwnLinuxDistro/script_1.sh
@@ -21,9 +21,11 @@ echo "127.0.0.1 ${HOSTNAME}.localdomain $HOSTNAME" >> /etc/hosts
 echo "chose your root password: "
 passwd 
 
-pacman -S dosfstools os-prober mtools network-manager-applet networkmanager wpa_supplicant wireless_tools dialog sudo
+pacman -S dosfstools os-prober ntools network-manager-applet networkmanager wpa_supplicant wireless_tools dialog sudo
 
 pacman -S grub-efi-x86_64 efibootmgr
+
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch_grub --recheck
+
 cp /usr/share/locale/en@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
 grub-mkconfig -o /boot/grub/grub.cfg
