2ef2d954256c8ce88de57fd630c252b5f6a3c4d7
joao99sb <joaovictor665@gmail.com>
2021-10-18 12:42:40 -0300

    create new user and desktop environment

--- OwnLinuxDistro/script_2.sh
+++ OwnLinuxDistro/script_2.sh
@@ -14,3 +14,29 @@ echo -e "password: "
 read -s PASS_WIFI
 
 nmcli device wifi connect "${WIFI}"  password "${PASS_WIFI}"
+
+#################
+#  add new user #
+#################
+
+echo "name of user: "
+read USER
+
+useradd -m ${USER}
+passwd ${USER}
+
+#add permitions manually
+
+nano /etc/sudoers
+
+##########################
+#  Desktop environments  #
+##########################
+
+pacmna -S xorg plasma plasma-wayland-session kde-applications --noconfirm
+
+systemctl enable sddm.service
+systemctl enable NetworkManager.service
+reboot
+
+
